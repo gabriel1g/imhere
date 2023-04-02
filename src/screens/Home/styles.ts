52dbe52873275aff2c75e49858623b1faba7edfb
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-02 00:33:40 -0300

    criação da Home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#131016',
+  },
+  eventName: {
+    marginTop: 48,
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
+});
