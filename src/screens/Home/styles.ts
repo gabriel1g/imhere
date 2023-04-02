61e3d8b95afe9cea78dc9db833cf71cb0c37c1bb
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-02 00:55:35 -0300

    formulário de adicionar participante

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,30 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  input: {
+    flex: 1,
+    height: 56,
+    padding: 16,
+    borderRadius: 5,
+    marginRight: 10,
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+  },
+  form: {
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+    width: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
 });
